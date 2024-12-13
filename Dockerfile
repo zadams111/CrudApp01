d9003fb8b0f4dc0a86b7d576090d4f0f548c9478
zadams1111 <73297253+zadams1111@users.noreply.github.com>
2024-12-12 21:38:32 -0500

    updated docker file

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM node:12
+FROM node:14
 
-WORKDIR /app
+WORKDIR /CrudApp01
 
 COPY package.json package-lock.json ./
 
--- database.js
+++ database.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
 }).promise();
 
 
